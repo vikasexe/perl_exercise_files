2068d8c600a626a34f575e566113779f5ba0db6b
vikasexe <vikas.exe@gmail.com>
2018-05-13 22:47:04 -0700

    Initial check-in

---
+++ 01_Quick_Start/hello.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -W
+# hello.pl by Bill Weinman <http://bw.org/contact/>
+# Copyright (c) 2010 The BearHeart Group, LLC
+#
+use strict;
+use warnings;
+
+$a = 'vikas';
+print "Hello, World! $a\n";
